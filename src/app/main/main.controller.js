917ed6eb4765666a4b3762c20e9f9feeacd79719
sundarcodes <sundar82@gmail.com>
2016-05-06 17:27:03 +0530

    Read Operation working for student and details

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -18,11 +18,42 @@
     }
    
      vm.showSections = function(klassId){
+      vm.sectionResp=null;
+      vm.studentResp=null;
+      vm.studentDetailResp=null;
+      vm.currentClassId = klassId;
       var resourceObj = $resource('http://pure-retreat-73401.herokuapp.com/api/v1/klasses/'+klassId+'/sections.json',{
         access_token:'TLVMLZCHEBSBAVTQJDV5LVTB7E8S74Q4'
       });
       vm.sectionResp = resourceObj.get();
      }
+
+     vm.showStudents = function(sectionId){
+      vm.studentResp=null;
+      vm.studentDetailResp=null;
+      vm.currentSectionId = sectionId;
+      var resourceObj = $resource('http://pure-retreat-73401.herokuapp.com/api/v1/klasses/'+vm.currentClassId+'/sections/'+sectionId+'/students.json',{
+        access_token:'TLVMLZCHEBSBAVTQJDV5LVTB7E8S74Q4'
+      });
+      vm.studentResp = resourceObj.get();
+     }
+
+     vm.showStudentDetails = function(studentId){
+      vm.currentStudentId = studentId;
+      var resourceObj = $resource('http://pure-retreat-73401.herokuapp.com/api/v1/klasses/'+vm.currentClassId+'/sections/'+vm.currentSectionId+'/students/'+studentId,{
+        access_token:'TLVMLZCHEBSBAVTQJDV5LVTB7E8S74Q4'
+      });
+      vm.studentDetailResp = resourceObj.get();
+      console.log(vm.studentDetailResp);
+     }
+
+     function resetAll(){
+      // vm.klassResp={};
+      vm.sectionResp=null;
+      vm.studentResp=null;
+      vm.studentDetailResp=null;
+     }
+
     init();
   }
 })();
